3c46ab6756ae225b1aabd0805dfed260439cddc6
Ashu <ashutoshsarangi95@gmail.com>
2020-10-02 07:36:56 +0530

    Update the Data in firebase Done and Deployed into FireBase.

--- Small Apps/todo-firebase-react/src/App.js
+++ Small Apps/todo-firebase-react/src/App.js
@@ -8,6 +8,8 @@ import firebase from 'firebase';
 function App() {
   const [todo, setTodo] = useState([{}]);
   const [input, setInput] = useState(['']);
+  const [button, setButton] = useState(['Add Todo']);
+  const [todoId, setTodoId] = useState(['']);
 
   /**
    * @description When it loads It Should get the Data From FireStore
@@ -28,21 +30,41 @@ function App() {
   // Add Operation in Firebase
   function addTodo(event) {
     event.preventDefault();
-    if (input.trim()) {
-      db.collection('todos').add({
-        todoName: input,
-        timestamp: firebase.firestore.FieldValue.serverTimestamp()
-      })
+    if (button === 'Add Todo') {
+      // FireBase Add Code
+      if (input.trim()) {
+        db.collection('todos').add({
+          todoName: input,
+          timestamp: firebase.firestore.FieldValue.serverTimestamp()
+        })
+      }
+      setInput('');
+    } else {
+      // FireBase Update Code
+      db.collection('todos').doc(todoId).set({
+        todoName: input
+      }, { merge: true });
+
+      setInput('');
+      setButton('Add Todo');
+      setTodoId('');
+
     }
-    setInput('');
   }
+  // Update / Edit TODO:
+  function updateTodo(item) {
+    setInput(item.todoName);
+    setButton('Update ToDo');
+    setTodoId(item.id);
+  }
+
   return (
     < div className="App" >
       <h1>WelCome to TODO + FIREBASE !!!</h1>
       <form>
         <input value={input} onChange={(event) => setInput(event.target.value)} />
         <Button disabled={!input} type='submit' onClick={(event) => addTodo(event)} variant="contained" color="primary">
-          ADD TODO
+          {button}
         </Button>
       </form>
       <ul>
@@ -51,6 +73,7 @@ function App() {
             <li key={item.id}>
               {item.todoName} --> {item.id}
               <button onClick={() => { removeTodo(item.id) }}>Delete Me</button>
+              <button onClick={() => { updateTodo(item) }}>Update Me</button>
             </li>
           ))
         }
