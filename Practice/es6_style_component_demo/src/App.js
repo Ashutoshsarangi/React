6bdbd810e1e803a654c1e092fe8bb49ce8f337ae
Ashu <ashutoshsarangi95@gmail.com>
2021-10-22 05:00:46 +0530

    added the es6 styled component

---
+++ Practice/es6_style_component_demo/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ChildComp1 from './childComp';
+
+function App() {
+  return (
+    <div className="App">
+      <ChildComp1 name="Ashutosh Sarangi"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Practice/es6_style_component_demo/src/childComp.jsx
@@ -0,0 +1,13 @@
+
+
+
+const ChildComp1 = (props)=>(
+    // const {name} = props;
+    <>
+    <h1>{props.name}</h1>
+    <h1>{props.name}</h1>
+    </>
+);
+
+
+export default ChildComp1;
